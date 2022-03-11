b9cc2363903d1503e26b2874e1908374bd666d8b
andremagne <andr.paulauskas@gmail.com>
2022-03-11 13:35:51 +0200

    Added comments and updated the code

--- app/src/main/java/edu/ktu/devicelocation/MainActivity.kt
+++ app/src/main/java/edu/ktu/devicelocation/MainActivity.kt
@@ -14,35 +14,37 @@ import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
-    val LOCATION_PERMISSION_REQUEST_CODE = 1;
+    // Permission request code to differentiate from other requests.
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        // Initiates the application's code.
         attemptDiscoverLocation();
     }
 
-    fun attemptDiscoverLocation() {
+    private fun attemptDiscoverLocation() {
         when {
+            // Check if permission is granted.
             ContextCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_COARSE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED -> {
+                // Continue with using the device's location.
                 discoverLocation()
             }
+            // Permission is not granted, should we show rationale?
             ActivityCompat.shouldShowRequestPermissionRationale(
                 this,
                 Manifest.permission.ACCESS_COARSE_LOCATION
             ) -> {
+                // Show rationale
                 showRationale();
             }
             else -> {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
-                    LOCATION_PERMISSION_REQUEST_CODE
-                )
+                // Permission not granted, make a request.
+                makeLocationPermissionRequest()
             }
         }
     }
@@ -53,42 +55,58 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // Check if this is the request code we're expecting.
         if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            // If the result array is empty, that means the user canceled the request.
+            // If it's not empty, we have to check if our request was granted.
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // If the request was granted, we continue with our execution.
                 discoverLocation();
             } else {
+                // If the request was denied or canceled, we explain why our application won't work.
                 showDenyMessage();
             }
         }
     }
 
-    fun discoverLocation() {
+    private fun discoverLocation() {
+        // Get the location manager service.
         val lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        // Retrieve the location, longitude and latitude.
         val location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER)
         val long = location?.longitude
         val lat = location?.latitude
-
+        // Show longitude and latitude on the screen.
         findViewById<TextView>(R.id.message_text).text =
             String.format(resources.getString(R.string.location), long, lat)
+    }
 
+    private fun makeLocationPermissionRequest() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+            LOCATION_PERMISSION_REQUEST_CODE
+        )
     }
 
     private fun showRationale() {
         AlertDialog.Builder(this).setMessage(R.string.location_rationale)
-            .setPositiveButton(R.string.ok) { _, _ ->
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
-                    LOCATION_PERMISSION_REQUEST_CODE
-                )
+            // The positive button dismisses the dialog.
+            .setPositiveButton(R.string.ok) { dialogInterface, _ ->
+                dialogInterface.dismiss()
+                // Once the dialog is dismissed a request for the permission is called.
+            }.setOnDismissListener {
+                makeLocationPermissionRequest()
             }.show()
     }
 
-    fun showDenyMessage() {
+    private fun showDenyMessage() {
         AlertDialog.Builder(this).setMessage(R.string.location_denied_message)
             .setPositiveButton(R.string.ok) { dialogInterface, _ ->
                 dialogInterface.dismiss()
             }.show()
+        // Set the text of the message informing the user
+        // that the device location can't be determined.
         findViewById<TextView>(R.id.message_text).text = resources.getString(R.string.no_location)
     }
 
