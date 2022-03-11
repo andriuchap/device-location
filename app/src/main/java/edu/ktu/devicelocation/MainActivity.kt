7eb6449b40874b6f25079f4f61a3a2f8217eb59d
andremagne <andr.paulauskas@gmail.com>
2022-03-11 12:26:43 +0200

    location permission is requested with activity result contract

--- app/src/main/java/edu/ktu/devicelocation/MainActivity.kt
+++ app/src/main/java/edu/ktu/devicelocation/MainActivity.kt
@@ -8,13 +8,21 @@ import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
-    val LOCATION_PERMISSION_REQUEST_CODE = 1;
+    val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                discoverLocation()
+            } else {
+                showDenyMessage()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,26 +46,7 @@ class MainActivity : AppCompatActivity() {
                 showRationale();
             }
             else -> {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
-                    LOCATION_PERMISSION_REQUEST_CODE
-                )
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                discoverLocation();
-            } else {
-                showDenyMessage();
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
             }
         }
     }
@@ -76,11 +65,7 @@ class MainActivity : AppCompatActivity() {
     private fun showRationale() {
         AlertDialog.Builder(this).setMessage(R.string.location_rationale)
             .setPositiveButton(R.string.ok) { _, _ ->
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
-                    LOCATION_PERMISSION_REQUEST_CODE
-                )
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
             }.show()
     }
 
